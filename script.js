34cb8f6c5b012691ce541a297db5e92c61f09284
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-09-29 19:24:07 -0300

    Alterações no HTML,CSS e inclusão de funções no Javascript.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let totalSliders = document.querySelectorAll('.slider--item').length;
 let currentSlide = 0;
+let sliderWidth = document.querySelector('.slider').clientWidth;
 
-document.querySelector('.slider--width').style.width = ` calc(100vw * ${totalSliders})`;
+document.querySelector('.slider--width').style.width = 
+` ${sliderWidth * totalSliders}`;
+
+document.querySelector('.slider--controls').style.width = 
+`${sliderWidth}px`;
+
 document.querySelector('.slider--controls').style.height = 
 `${document.querySelector('.slider').clientHeight}px`;
 
@@ -22,9 +28,10 @@ function goNext() {
 }
 
 function updateMargin() {
-   let newMargin = (currentSlide * document.body.clientWidth);
-   document.querySelector('.slider--width').style.marginLeft = 
-   `-${newMargin}px`;
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+    let newMargin = (currentSlide * sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft = 
+        `-${newMargin}px`;
 }
 
-setTimeout(goNext ,2000)
+setInterval(goNext,5000)
